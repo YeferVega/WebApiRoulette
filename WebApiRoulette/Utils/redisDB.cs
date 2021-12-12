9746d0866673de599ce42dc8f1eda371cbc6d84b
Yefer Vega <jeff.4.2@hotmail.com>
2021-12-12 13:25:43 -0500

    Add project files.

---
+++ WebApiRoulette/Utils/redisDB.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiRoulette
+{
+    public class redisDB
+    {
+        public class connection
+        {
+            private static Lazy<ConnectionMultiplexer> _lazyConnection;
+            public static ConnectionMultiplexer Connection
+            {
+                get {          
+                    
+                    return _lazyConnection.Value;
+                }
+            }
+            static connection() {
+
+                _lazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect("localhost"));
+            }
+
+        }
+        
+        
+    }
+}
